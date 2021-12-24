021ae2428fd5776d83504d0895c3f459a67d4341
LucasGabryellll <gabryelmonteiro1@gmail.com>
2021-12-24 14:47:07 -0300

    Images the Game

---
+++ res/tileFloresta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="tileFloresta" tilewidth="32" tileheight="32" tilecount="1496" columns="8">
+ <image source="tileFloresta.png" width="256" height="6000"/>
+</tileset>
